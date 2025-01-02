75a183d5bc200a1abf8ff3e39f2c6958979cb544
asr1027 <1062642212@qq.com>
2025-01-03 00:24:09 +0800

    后端:SpringBoot拦截器与分页插件配置

--- back/src/main/java/com/config/MybatisPlusConfig.java
+++ back/src/main/java/com/config/MybatisPlusConfig.java
@@ -14,7 +14,7 @@ import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
 public class MybatisPlusConfig {
 
     /**
-     * 分页插件
+     * 分页插件框架
      */
     @Bean
     public PaginationInterceptor paginationInterceptor() {
